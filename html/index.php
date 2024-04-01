6a52b59fe2c05e58276b96df5ec7bce2e254d9d3
ShadyMate <luisboehler@proton.me>
2024-04-01 11:12:58 +0200

    Added right hand side list for Items, function to add Items with color, hardcoded Items

--- html/index.php
+++ html/index.php
@@ -6,10 +6,16 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 		<link rel="stylesheet" href="../src/css/reset.css" />
 		<link rel="stylesheet" href="../src/css/main.css" />
+        <link rel="stylesheet" href="../src/css/style.css" />
 		<link
 			rel="icon"
 			href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>⚔️</text></svg>"
 		/>
+        <style>
+            main {
+                display: flex;
+            }
+        </style>
 		<title>RPG Inventory | Simple Character Inventory Design</title>
 		<script defer src="../src/js/main.js"></script>
 	</head>
@@ -118,6 +124,35 @@
 					<div class="inventory-slot" data-slot="99" draggable="true"></div>
 				</div>
 			</div>
+            <div class="sidebar">
+                <label for="itemName"></label><input id="itemName" type="text" placeholder="Name des Items">
+                <label for="itemColor"></label><input id="itemColor" type="color">
+                <button onclick="addItem()">Add Item</button>
+                <div id="itemlist">
+                    <div class="item" style="color: blue">Battleaxe</div>
+                    <div class="item" style="color: blue">Dagger</div>
+                    <div class="item" style="color: blue">Shield</div>
+                    <div class="item" style="color: blue">Shortsword</div>
+                    <div class="item" style="color: blue">Spear</div>
+                    <div class="item" style="color: blue">Backpack</div>
+                    <div class="item" style="color: green">Shortbow</div>
+                    <div class="item" style="color: green">Longbow</div>
+                    <div class="item" style="color: green">Crossbow</div>
+                </div>
+            </div>
+            <script>
+
+                function addItem(){
+                    var newItem = document.createElement("div");
+                    var itemName = document.getElementById("itemName").value;
+                    var itemColor = document.getElementById("itemColor").value;
+                    newItem.textContent = itemName;
+                    newItem.ClassName = "item";
+                    newItem.style.color = itemColor;
+                    var list = document.getElementById("itemlist");
+                    list.insertBefore(newItem, list.childNodes[0]);
+                }
+            </script>
 		</main>
 	</body>
 </html>
