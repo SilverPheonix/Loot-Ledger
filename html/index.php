e2c00cc7f31d5ee1949284e42fab12930cfe81fd
ShadyMate <luisboehler@proton.me>
2024-04-14 18:18:13 +0200

    login.php profile.php and register.php added

--- html/index.php
+++ html/index.php
@@ -6,7 +6,6 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 		<link rel="stylesheet" href="../src/css/reset.css" />
 		<link rel="stylesheet" href="../src/css/main.css" />
-        <link rel="stylesheet" href="../src/css/style.css" />
 		<link
 			rel="icon"
 			href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>⚔️</text></svg>"
