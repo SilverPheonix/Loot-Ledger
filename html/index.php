29835720b093fca2c3673c782ab87c4a5f409e0f
Elaine <elaine.fink19@gmail.com>
2024-04-15 16:53:05 +0200

    item drag and drop

--- html/index.php
+++ html/index.php
@@ -4,6 +4,8 @@
 		<meta charset="UTF-8" />
 		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 		<link rel="stylesheet" href="../src/css/reset.css" />
 		<link rel="stylesheet" href="../src/css/main.css" />
 		<link
@@ -21,9 +23,9 @@
 	<body>
 		<?php include "navbar.php";?>
 		<main>
-			<div class="character-inventory">
+			<?php include "characterlist.php";?>
+			<div class="character-inventory col-md-6">
 				<h1>Loot Ledger</h1>
-				<?php include "characterlist.php";?>
 				<div class="character-inventory-slots">
 					<div class="inventory-slot" data-slot="0-1-10-11" draggable="true">⚔️</div>
 					<div class="inventory-slot" data-slot="2" draggable="true">⚔️</div>
--- html/itemlist.php
+++ html/itemlist.php
@@ -1,17 +1,119 @@
-<div class="sidebar">
+            
+            <div class="sidebar col-md-3">
+                <div class = container>
                 <label for="itemName"></label><input id="itemName" type="text" placeholder="Name des Items">
                 <label for="itemColor"></label><input id="itemColor" type="color">
-                <button onclick="addItem()">Add Item</button>
+                <button onclick="addItem()">Add Item</button><br><hr><br>
                 <div id="itemlist">
-                    <div class="item" style="color: blue">Battleaxe</div>
-                    <div class="item" style="color: blue">Dagger</div>
-                    <div class="item" style="color: blue">Shield</div>
-                    <div class="item" style="color: blue">Shortsword</div>
-                    <div class="item" style="color: blue">Spear</div>
-                    <div class="item" style="color: blue">Backpack</div>
-                    <div class="item" style="color: green">Shortbow</div>
-                    <div class="item" style="color: green">Longbow</div>
-                    <div class="item" style="color: green">Crossbow</div>
+                    <div class="card">
+                        <div class="card-header">
+                            <a class="btn" data-bs-toggle="collapse" href="#battleaxe" style="color: blue">Battleaxe</a>
+                        </div>
+                        <div id="battleaxe" class="collapse">
+                            <div class="card-body">
+                                <div class="inventory-slot" data-slot="101"draggable="true">
+                                    <img  width=100% src="../src/img/items/Battleaxe.jpeg">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <div class="card-header">
+                            <a class="btn" data-bs-toggle="collapse" href="#dagger" style="color: blue">Dagger</a>
+                        </div>
+                        <div id="dagger" class="collapse">
+                            <div class="card-body">
+                                <div class="inventory-slot" data-slot="102"draggable="true">
+                                    <img  width=100% src="../src/img/items/Dagger1.jpeg">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <div class="card-header">
+                            <a class="btn" data-bs-toggle="collapse" href="#shield" style="color: blue">Shield</a>
+                        </div>
+                        <div id="shield" class="collapse">
+                            <div class="card-body">
+                                <div class="inventory-slot" data-slot="103"draggable="true">
+                                    <img  width=100% src="../src/img/items/Shield.jpeg">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <div class="card-header">
+                            <a class="btn" data-bs-toggle="collapse" href="#shortsword" style="color: blue">Shortsword</a>
+                        </div>
+                        <div id="shortsword" class="collapse">
+                            <div class="card-body">
+                                <div class="inventory-slot" data-slot="104"draggable="true">
+                                    <img  width=100% src="../src/img/items/Shortsword.jpeg">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <div class="card-header">
+                            <a class="btn" data-bs-toggle="collapse" href="#Spear" style="color: blue">Spear</a>
+                        </div>
+                        <div id="Spear" class="collapse">
+                            <div class="card-body">
+                                <div class="inventory-slot" data-slot="105"draggable="true">
+                                    <img  width=100% src="../src/img/items/Spear.jpeg">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <div class="card-header">
+                            <a class="btn" data-bs-toggle="collapse" href="#Backpack" style="color: blue">Backpack</a>
+                        </div>
+                        <div id="Backpack" class="collapse">
+                            <div class="card-body">
+                                <div class="inventory-slot" data-slot="106"draggable="true">
+                                    <img  width=100% src="../src/img/items/Backpack.jpeg">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <div class="card-header">
+                            <a class="btn" data-bs-toggle="collapse" href="#Shortbow" style="color: green">Shortbow</a>
+                        </div>
+                        <div id="Shortbow" class="collapse">
+                            <div class="card-body">
+                                <div class="inventory-slot" data-slot="107"draggable="true">
+                                    <img  width=100% src="../src/img/items/Shortbow.jpeg">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <div class="card-header">
+                            <a class="btn" data-bs-toggle="collapse" href="#Longbow" style="color: green">Longbow</a>
+                        </div>
+                        <div id="Longbow" class="collapse">
+                            <div class="card-body">
+                                <div class="inventory-slot" data-slot="108"draggable="true">
+                                    <img  width=100% src="../src/img/items/Longbow.jpeg">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <div class="card-header">
+                            <a class="btn" data-bs-toggle="collapse" href="#Crossbow" style="color: green">Crossbow</a>
+                        </div>
+                        <div id="Crossbow" class="collapse">
+                            <div class="card-body">
+                                <div class="inventory-slot" data-slot="109"draggable="true">
+                                    <img  width=100% src="../src/img/items/Crossbow.jpeg">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
                 </div>
             </div>
             <script>
