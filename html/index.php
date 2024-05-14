53f94a823b44239a23370e98404a2d02e436f140
kevin <if23b277@technikum-wien.at>
2024-05-14 03:23:17 +0200

    implemented character creation and deletion

--- html/index.php
+++ html/index.php
@@ -6,6 +6,7 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 		<link rel="stylesheet" href="../src/css/reset.css" />
 		<link rel="stylesheet" href="../src/css/main.css" />
 		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
@@ -25,6 +26,11 @@
 		<?php include "navbar.php";?>
 		<main>
 			<?php include "characterlist.php";?>
+
+			<div class="character-create">
+				<button onclick="createCharacter()">Create Character</button>
+			</div>
+			
 			<div class="character-inventory col-md-6">
 				<h1>Loot Ledger</h1>
 				<div class="character-inventory-slots">
@@ -130,7 +136,41 @@
 					<div class="inventory-slot" data-slot="98" draggable="true"></div>
 					<div class="inventory-slot" data-slot="99" draggable="true"></div>
 				</div>
-			</div>
+			</div>	
+			
+				<script>
+					function createCharacter() {
+						var characterName = prompt("Enter character name:");
+						var characterStrength = prompt("Enter character strength:");
+						if (characterName != null && characterName != "" && characterStrength != null && characterStrength != "") {
+							
+							var userId = <?php echo $_SESSION['id']; ?>;
+							insert_character(userId, characterName, characterStrength);
+						}
+					}
+
+					function insert_character(userId, characterName, characterStrength) {	
+						$.ajax({
+								url: '../src/db/insertCharacter.php',
+								method: 'POST',
+								data: { 
+									user_id: userId,
+									name: characterName,
+									strength: characterStrength
+								},
+								success: function() {
+									//debug
+									console.log('Character uploaded successfully'+ userId + characterName + characterStrength);
+								},
+								error: function(error) {
+									console.error('Error uploading character:' + userId + characterName + characterStrength, error);
+								}
+							});
+					}
+				</script>
+				
+				
+				
 			<?php include "itemlist.php";?>
 		</main>
 	</body>
