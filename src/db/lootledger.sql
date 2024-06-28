dc2e1e30a5f24574b58449cd09b575df8f4033d4
nbrandtner-tgm <nbrandtner@gmx.at>
2024-06-28 13:39:39 +0200

    add finished user manual

--- src/db/lootledger.sql
+++ src/db/lootledger.sql
@@ -23,9 +23,7 @@ CREATE TABLE `user` (
 
 INSERT INTO `user` (`u_id`, `u_firstname`, `u_lastname`, `u_role`, `u_pw`, `u_status`, `u_email`, `u_username`) VALUES
 (1, 'admin', 'admin', 'admin', '$2y$10$oKGo5XN3i68qI77iQ3xZM.3alePgCmiuZN4NFOQAg9HOzEPf5COGK', 1, 'admin@admin.com', 'admin'),
-(2, 'user', 'user', 'user', '$2y$10$zFhc0qlyE66r7CSu1fwU7.VgEBA8r8xewniiDlMzfNH3kNveJz122', 1, 'user@user.com', 'user'),
-(3, 'Sonja', 'Lechner', 'user', '$2y$10$nJhz03QirtTwrha93d1ZjOSWiVXqUgBphWyJK3kTx4AcMDvu0QIaK', 1, 'sonja-lechner@gmail.com', 'nomoresonja'),
-(8, 'Niklas', 'Brandtner', 'user', '$2y$10$qDkJ2MJbIi4nvdNTmnetSeUWzfMAMdWYcSTsjRj5KmSWbZNziz.iG', 1, 'nbrandtner@gmx.at', 'Yukii');
+(2, 'user', 'user', 'user', '$2y$10$zFhc0qlyE66r7CSu1fwU7.VgEBA8r8xewniiDlMzfNH3kNveJz122', 1, 'user@user.com', 'user');
 
 DROP TABLE IF EXISTS `characters`;
 CREATE TABLE `characters` (
